0bfaf5f9dc5c93de59a1544e34b2a5f18331f426
dmitruez <dyezerski@icloud.com>
2024-02-03 02:31:50 +0300

    new func for bot information

--- repository.py
+++ repository.py
@@ -1,12 +1,12 @@
 import json
-from entities import Button, User, GroupButtons
+from entities import Button, ButtonsGroup, BotText, BotTextGroup, User
 
 
 class RepoJSON:
 	FILE = 'config.json'
 	
 	
-	def buttons(self) -> GroupButtons:
+	def buttons(self) -> ButtonsGroup:
 		buttons_list = []
 		with open(self.FILE, 'r', encoding='utf-8') as f:
 			data = json.load(f)['buttons']
@@ -20,7 +20,7 @@ class RepoJSON:
 				
 				buttons_list.append(a)
 			
-			buttons = GroupButtons(buttons_list)
+			buttons = ButtonsGroup(buttons_list)
 			
 		return buttons
 	
@@ -37,34 +37,28 @@ class RepoJSON:
 		return info
 	
 	
+	def about_bot(self) -> BotTextGroup:
+		bot_info = []
+		with open(self.FILE, 'r', encoding='utf-8') as f:
+			data = json.load(f)['about_bot']
+			for option in data['bot_text']:
+				a = BotText(
+					name=option,
+					ru=data['bot_text'][option]['ru'],
+					en=data['bot_text'][option]['en']
+					)
+				bot_info.append(a)
+				
+			bot = BotTextGroup(bot_info)
+			
+		return bot
+	
+	
 	def token(self):
 		with open(self.FILE, 'r', encoding='utf-8') as f:
 			token = json.load(f)['about_bot']['TOKEN']
 		
 		return token
 	
-	
-	def fist_greetings(self, lang):
-		with open(self.FILE, 'r', encoding='utf-8') as f:
-			data = json.load(f)['about_bot']['greetings']
-			if lang == 'ru':
-				greetings = data['ru']
-			
-			else:
-				greetings = data['en']
-		
-		return greetings
-	
-	
-	def text_question(self, lang):
-		with open(self.FILE, 'r', encoding='utf-8') as f:
-			data = json.load(f)['about_bot']['text_question']
-			if lang == 'ru':
-				question = data['ru']
-			
-			else:
-				question = data['en']
-		
-		return question
 
 repo = RepoJSON()
