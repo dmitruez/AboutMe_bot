50e2f31c7bfca87d80dc99c8d1251d64f85d23be
dmitruez <dyezerski@icloud.com>
2024-02-02 22:36:41 +0300

    handler updated for new entities

--- handlers.py
+++ handlers.py
@@ -1,4 +1,3 @@
-from aiogram.fsm.state import State
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -8,17 +7,23 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 from services import ServiceBot
 from entities import User
-
+from states import States
 
 service = ServiceBot()
 repo = RepoJSON()
 router = Router()
-send_message_to_me = State()
+buttons = repo.buttons()
 
 @router.message(Command("start"))
 async def greetings(message: Message):
 	lang = message.from_user.language_code
-	call_data, text = repo.button_info('show_information', lang)
+	button = buttons.show_information
+	if lang == 'ru':
+		text = button.ru
+	else:
+		text = button.en
+	call_data = button.call_data
+	
 	button = [[InlineKeyboardButton(text=text, callback_data=call_data)]]
 	user = User(
 		message.from_user.first_name, message.from_user.username, message.from_user.id, message.from_user.language_code
@@ -30,21 +35,29 @@ async def greetings(message: Message):
 @router.callback_query(F.data == 'info')
 async def starting_info(clbck: CallbackQuery, state: FSMContext, page=0):
 	lang = clbck.from_user.language_code
+	button_next = buttons.next_fact
+	button_question = buttons.question_to_me
+	button_back = buttons.previous_fact
+	
 	facts = repo.information(lang)
 	pages_count = len(facts) - 1
 	
-	call_data_next, text_next = repo.button_info('next_fact', lang)
-	call_data_question, text_question = repo.button_info('question_to_me', lang)
-	call_data_back, text_back = repo.button_info('previous_fact', lang)
 	
 	left = page - 1 if page != 0 else pages_count
 	right = page + 1 if page != pages_count else 0
 	
-	buttons = [[
-		InlineKeyboardButton(text=text_back, callback_data=call_data_back + str(left)),
-		InlineKeyboardButton(text=text_next, callback_data=call_data_next + str(right))
-		], [InlineKeyboardButton(text=text_question, callback_data=call_data_question)]]
-	markup = InlineKeyboardMarkup(inline_keyboard=buttons, row_widht=1)
+	if lang == 'ru':
+		
+		but = [[
+			InlineKeyboardButton(text=button_back.ru, callback_data=button_back.call_data + str(left)),
+			InlineKeyboardButton(text=button_next.ru, callback_data=button_next.call_data + str(right))],
+			[InlineKeyboardButton(text=button_question.ru, callback_data=button_question.call_data)]]
+	else:
+		but = [[
+			InlineKeyboardButton(text=button_back.en, callback_data=button_back.call_data + str(left)),
+			InlineKeyboardButton(text=button_next.en, callback_data=button_next.call_data + str(right))],
+			[InlineKeyboardButton(text=button_question.en, callback_data=button_question.call_data)]]
+	markup = InlineKeyboardMarkup(inline_keyboard=but, row_widht=1)
 	
 	await clbck.message.edit_text(text=facts[page], reply_markup=markup)
 	await clbck.answer()
@@ -55,27 +68,44 @@ async def next_fact(clbck: CallbackQuery, state: FSMContext):
 	lang = clbck.from_user.language_code
 	text_quest = repo.text_question(lang)
 	if str(clbck.data).startswith('to '):
-		
 		page = int(str(clbck.data).split(' ')[1])
-		
 		await starting_info(clbck, state, page=page)
-	else:
-		await state.set_state(send_message_to_me)
+		
+	elif clbck.data == 'question':
+		await state.set_state(States.send_message_to_me)
 		await clbck.message.answer(text=text_quest)
 		
+	elif clbck.data == 'reply':
+		await state.set_state(States.reply_message)
+		await clbck.message.bot.send_message(2048360747, 'Отправьте ответ пользователю')
 		
-@router.message(send_message_to_me)
+		
+@router.message(States.send_message_to_me)
 async def send_to_me(message: Message):
 	username = message.from_user.first_name
 	url = message.from_user.url
 	lang = message.from_user.language_code
+	button_reply = buttons.reply_to_message
 	question = message.text
-	
-	call_data_reply, text_reply = repo.button_info("reply_to_message", lang)
-	button = [[InlineKeyboardButton(text=text_reply, callback_data=call_data_reply)]]
+	if lang == 'ru':
+		text = button_reply.ru
+	else:
+		text = button_reply.en
+		
+	call_data = button_reply.call_data
+	button = [[InlineKeyboardButton(text=text, callback_data=call_data)]]
 	keyboard = InlineKeyboardMarkup(inline_keyboard=button)
 	await message.bot.send_message(2048360747, text=
 													f'<b>Пользователь:</b>  <a href="{url}">{username}</a>\n'
 	                                                f'<b>Язык:</b>  {lang}\n'
 	                                                f'<b>Вопрос:</b>  {question}', reply_markup=keyboard)
-	
+
+
+@router.message(States.reply_message)
+async def reply_message(message: Message):
+	lang = message.from_user.language_code
+	question, answer = message.text.split(': ')
+	if lang == 'ru':
+		await message.answer(text=f'<b>Вопрос:</b> {question}\n\n<b>Ответ:</b> {answer}')
+	else:
+		await message.answer(text=f'<b>Question:</b> {question}\n\n<b>Answer:</b> {answer}')
